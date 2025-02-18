1a9bef2d9bc2fcc86d4e0a8e52bfe70f86d03f4a
PythonMonty <andrewnovosel90@gmail.com>
2025-02-18 16:17:08 -0500

    Games page is up. However, need to figure out to display games list for users to join

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,5 +1,5 @@
 import { useState, React, useEffect } from "react";
-//import axios from "axios";
+import axios from "axios";
 import { Text, Button, Box, Link, Input } from '@chakra-ui/react';
 
 const apiURL = import.meta.env.VITE_API_URL;
@@ -42,22 +42,24 @@ function Game({ userId }) {
     })
   }
 
+  const joinGame = (gameId) => {}
+
   return (
     <Box>
       <Text color='black' fontSize='2xl' fontWeight='bold' marginBottom='1rem'>Games</Text>
       <form onSubmit={submitGame}>
         <Input type="text" width='200px' placeholder="Enter game name" onChange={addGame} value={gameInput} />
-        <Button type='submit' marginLeft='5px' marginBottom='5px' width='70px' fontWeight='bold' onClick={submitGame}>Join Game</Button>
+        {/* <Button type='submit' marginLeft='5px' marginBottom='5px' width='85px' fontWeight='bold' onClick={submitGame}>Join Game</Button> */}
         <Text fontWeight='bold' fontSize='xl' marginTop='0.5rem' color='black'>Join a game below</Text>
       </form>
       <ul>
-        {games.map(game => {
+        {/* {joinGame.map(game => {
           return (
             <li key={game.id}>
               <Link variant='plain' _hover={{textDecoration: 'underline', color: 'blue.600'}} href={`/users/${userId}/games/${game.id}`} color='black'>{game.name}</Link>
             </li>
           )
-        })}
+        })} */}
       </ul>
     </Box>
   )
