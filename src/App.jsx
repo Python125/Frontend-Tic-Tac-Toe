5d94c434f9107101a030c6506791b264b2a77326
PythonMonty <andrewnovosel90@gmail.com>
2025-02-21 13:11:06 -0500

    Removed wallet address input and related functionality

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ console.log(`API URL: ${apiURL}`);
 function App() {
   const [users, setUsers] = useState([]);
   const [userInput, setUserInput] = useState('');
-  const [walletAddress, setWalletAddress] = useState('');
+  // const [walletAddress, setWalletAddress] = useState('');
   
   useEffect(() => {
     const fetchUsers = async () => {
@@ -22,9 +22,9 @@ function App() {
     setUserInput(e.target.value);
   }
 
-  function addWalletAddress(e) {
-    setWalletAddress(e.target.value);
-  }
+  // function addWalletAddress(e) {
+  //   setWalletAddress(e.target.value);
+  // }
 
   function submitUser(e) {
     e.preventDefault();
@@ -33,7 +33,7 @@ function App() {
     const newUser = {
       id: users.length + 1,
       username: userInput,
-      walletAddress: walletAddress,
+      // walletAddress: walletAddress,
       games: [],
     }
     console.log(newUser);
@@ -41,7 +41,7 @@ function App() {
     axios.post(`${apiURL}/users`, newUser).then(response => {
       setUsers([...users, response.data]);
       setUserInput('');
-      setWalletAddress('');
+      // setWalletAddress('');
     })
   }
 
@@ -50,7 +50,7 @@ function App() {
       <Text color='black' fontSize='2xl' fontWeight='bold' marginBottom='1rem'>Login to your account</Text>
       <form onSubmit={submitUser}>
         <Input type="text" width='200px' placeholder="Enter username" value={userInput} onChange={addUser} />
-        <Input type="text" width='200px' placeholder="Enter wallet address" value={walletAddress} onChange={addWalletAddress} />
+        {/* <Input type="text" width='200px' placeholder="Enter wallet address" value={walletAddress} onChange={addWalletAddress} /> */}
         <Button type='submit' marginLeft='5px' marginBottom='5px' width='70px' fontWeight='bold' onClick={submitUser}>Login</Button>
         <Text fontWeight='bold' fontSize='xl' marginTop='0.5rem' color='black'>Find your username below</Text>
       </form>
