5c3295dd53442fcf26d4c541e6825d0a5a99ddbd
PythonMonty <andrewnovosel90@gmail.com>
2025-02-24 10:39:32 -0500

    Letting users choose player

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -5,11 +5,37 @@ import { Text, Button, Box, Link, Input } from '@chakra-ui/react';
 const apiURL = import.meta.env.VITE_URL;
 console.log('API URL:', apiURL);
 
-function Game() {
+function Game({ gameId }) {
+    const [gameSession, setGameSession] = useState([]);
+    const [player1, setPlayer1] = useState('');
+    const [player2, setPlayer2] = useState('');
+
+    useEffect(() => {
+        const fetchGameSession = async () => {
+            const response = await axios.get(`${apiURL}/games/${gameId}`);
+            setGameSession(response.data);
+        }
+        fetchGameSession();
+    }, [gameId]);
+
+    function choosePlayer() {
+        if (player1 === 'X') {
+            setPlayer1('X');
+            setPlayer2('O');
+        } else if (player1 === 'O') {
+            setPlayer1('O');
+            setPlayer2('X');
+        }
+    }
 
     return (
         <Box>
-            <Text fontSize="2xl" fontWeight="bold">Welcome to your game session</Text>
+            <Text fontWeight='bold' fontSize='2xl'>Welcome to your game session</Text>
+            <form onSubmit={choosePlayer}>
+                <Text fontWeight='bold' fontSize='xl' marginTop='0.5rem' color='black'>Choose your player</Text>
+                <Button type="submit" variant='solid' colorScheme='blue' marginRight='1rem' marginTop='1rem'>Player 1</Button>
+                <Button type="submit" variant='solid' colorScheme='blue' marginRight='1rem' marginTop='1rem'>Player 2</Button>
+            </form>
         </Box>
     )
 }
