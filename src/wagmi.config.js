989330cd5072954927d00c8f054f0571f9332330
PythonMonty <andrewnovosel90@gmail.com>
2025-03-02 10:05:15 -0500

    Simplified Wagmi configuration

--- src/wagmi.config.js
+++ src/wagmi.config.js
@@ -2,17 +2,14 @@
 
 import { http, createConfig } from 'wagmi';
 import { base, mainnet } from 'wagmi/chains';
-import { injected, metaMask, safe, walletConnect } from 'wagmi/connectors';
+import { metaMask } from 'wagmi/connectors';
 
 const projectId = '123';
 
 export const config = createConfig({
   chains: [mainnet, base],
   connectors: [
-    // injected(),
-    // walletConnect({ projectId }),
     metaMask(),
-    // safe(),
   ],
   transports: {
     [mainnet.id]: http(),
