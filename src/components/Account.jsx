091ed994eea60e6f861d908c500cea737115eae2
PythonMonty <andrewnovosel90@gmail.com>
2025-03-04 17:00:22 -0500

    Added back account.jsx file, with uppercase "A"

---
+++ src/components/Account.jsx
@@ -0,0 +1,17 @@
+import { useAccount, useDisconnect, useEnsAvatar, useEnsName } from 'wagmi';
+import { Box, Button } from '@chakra-ui/react';
+
+export function Account() {
+    const { disconnect } = useDisconnect();
+    const { address } = useAccount();
+    const { data: ensName } = useEnsName({ address });
+    const { data: ensAvatar } = useEnsAvatar({ address });
+
+    return (
+        <Box>
+            {ensAvatar && <img src={ensAvatar} alt='ENS Avatar' />}
+            {address && <div>{ensName ? `${ensName} (${address})` : address}</div>}
+            <Button onClick={() => disconnect()}>Disconnect</Button>
+        </Box>
+    )
+}
