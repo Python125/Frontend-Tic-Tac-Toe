d025a9aa9abccd720ad496fee2ca4879461e7590
PythonMonty <andrewnovosel90@gmail.com>
2025-03-24 13:41:52 -0400

    Added a BASIC dialog component using chakra-ui

--- src/components/GamesPage.jsx
+++ src/components/GamesPage.jsx
@@ -1,11 +1,11 @@
 import { useState, React, useEffect } from "react";
 import axios from "axios";
-import { Text, Input, Box, Link, Button } from '@chakra-ui/react';
+import { Text, Input, Box, Link, Button, Dialog, Portal } from '@chakra-ui/react';
 // import { QueryClient } from '@tanstack/react-query';
 import { useAccount } from 'wagmi';
 import AuthHeader from './AuthHeader';
 
-function GameList() {
+function GamesPage() {
   const [games, setGames] = useState([]);
   const [gameInput, setGameInput] = useState('');
   const { isConnected } = useAccount();
@@ -48,14 +48,27 @@ function GameList() {
   return (
     <>
       <AuthHeader />
-      <Box backgroundColor='gray.900' height='100%' color='white' display='flex' justifyContent='space-between' padding='0 700px'>
+      <Box backgroundColor='gray.900' color='white' display='flex' justifyContent='space-between' padding='0 500px'>
         {isConnected && (
           <>
             <Text fontWeight='300' fontSize='2xl' marginTop='20px'>Available Games</Text>
-            <Button backgroundColor='gray.900' color='white' border='1px solid white' borderRadius='md' marginTop='20px'>Create Challenge</Button>
-            {/* <form onSubmit={submitGame}>
-              <Input color='white' type="text" width='200px' marginBottom='10px' marginTop='10px' placeholder="Enter new game" onChange={addGame} value={gameInput} />
-            </form> */}
+
+            <Dialog.Root>
+              <Dialog.Trigger asChild>
+                <Button backgroundColor='gray.900' color='white' border='1px solid white' borderRadius='md' marginTop='20px' size='lg'>Create Challenge</Button>
+              </Dialog.Trigger>
+              <Dialog.Backdrop />
+              <Dialog.Positioner>
+                <Dialog.Content border='1px solid white' backgroundColor='gray.700' color='white' width='500px'>
+                  <Dialog.Body>
+                    <Input color='white' type="text" marginBottom='10px' marginTop='10px' placeholder="Enter new game" onChange={addGame} value={gameInput} />
+                  </Dialog.Body>
+                  <Dialog.Footer justifyContent='center'>
+                    <Button backgroundColor='gray.900' color='white' border='1px solid white' borderRadius='md' marginTop='20px' size='lg'>Create Challenge</Button>
+                  </Dialog.Footer>
+                </Dialog.Content>
+              </Dialog.Positioner>
+            </Dialog.Root>
           </>
         )}
       </Box>
@@ -79,4 +92,4 @@ function GameList() {
   )
 }
 
-export default GameList;
+export default GamesPage;
