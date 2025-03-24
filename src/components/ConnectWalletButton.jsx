0b6812a503771939de8a5ac20dde934a1bc773cf
PythonMonty <andrewnovosel90@gmail.com>
2025-03-24 10:12:00 -0400

    Applied Figma styles to home page, header, and buttons

--- src/components/ConnectWalletButton.jsx
+++ src/components/ConnectWalletButton.jsx
@@ -100,13 +100,7 @@ function ConnectWalletButton() {
     return (
         <div>
             {connectors.map((connector) => (
-                <Button
-                    backgroundColor="white" color="black"
-                    key={connector.id}
-                    onClick={() => connect({ connector })}
-                >
-                    Connect Wallet
-                </Button>
+                <Button backgroundColor="white" color="black" key={connector.id} onClick={() => connect({ connector })}>Connect Wallet</Button>
             ))}
         </div>
     )
