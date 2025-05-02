5aea0f8763c29e02b7f6bd1b22eb6af3712b9ef4
PythonMonty <andrewnovosel90@gmail.com>
2025-05-02 10:20:08 -0400

    Updated socket connection logic to use VITE_SOCKET_URL in production mode. Enhanced logging for socket connection and configured socket options for transport and credentials.

--- src/socket.js
+++ src/socket.js
@@ -1,11 +1,15 @@
 import { io } from 'socket.io-client';
 
-const apiURL = import.meta.env.MODE === 'production' ? undefined : import.meta.env.VITE_URL;
+const apiURL = import.meta.env.MODE === 'production' ? import.meta.env.VITE_SOCKET_URL : import.meta.env.VITE_URL;
 
-if (import.meta.env.MODE !== 'production') {
-    console.log('Socket URL:', apiURL);
-}
+console.log('Connecting socket to:', apiURL);
+
+// if (import.meta.env.MODE !== 'production') {
+//     console.log('Socket URL:', apiURL);
+// }
 
 export const socket = io(apiURL, {
     autoConnect: false,
+    transports: ['websocket'],
+    withCredentials: true,
 });
