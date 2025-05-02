664ef847510d36bbd5edce13adade0a2a359c885
PythonMonty <andrewnovosel90@gmail.com>
2025-05-02 15:02:42 -0400

    Refactored socket connection logic to always use VITE_URL, removing production mode condition for improved consistency.

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const apiURL = import.meta.env.MODE === 'production' ? undefined : import.meta.env.VITE_URL;
+const apiURL = import.meta.env.VITE_URL;
 
 if (import.meta.env.MODE !== 'production') {
     console.log('Socket URL:', apiURL);
