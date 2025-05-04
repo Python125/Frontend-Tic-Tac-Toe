5b99fffc70d13778dedf76d59f4c59b118b81b61
PythonMonty <andrewnovosel90@gmail.com>
2025-05-04 13:58:47 -0400

    Updated Toaster component to include marginTop property for improved spacing in the UI.

--- src/components/ui/toaster.jsx
+++ src/components/ui/toaster.jsx
@@ -38,7 +38,7 @@ export const toaster = createToaster({
 export const Toaster = () => {
   return (
     <Portal>
-      <ChakraToaster toaster={toaster} insetInline={{ mdDown: '4' }}>
+      <ChakraToaster toaster={toaster} insetInline={{ mdDown: '4' }} marginTop='50px'>
         {(toast) => (
           <Toast.Root width={{ md: 'sm' }}>
             {toast.type === 'loading' ? (
